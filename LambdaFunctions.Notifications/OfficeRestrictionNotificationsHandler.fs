70002b6ca3ca6ddee653240c1a4363f086ef537b
Steve Renard <steve.renard@broadsign.com>
2022-06-05 16:34:04 +0200

    Renamed event

--- LambdaFunctions.Notifications/OfficeRestrictionNotificationsHandler.fs
+++ LambdaFunctions.Notifications/OfficeRestrictionNotificationsHandler.fs
@@ -3,18 +3,16 @@
 open Amazon.Lambda.Core
 open System.Text.Json
 open LambdaFunctions.Facade
-type MyEvent =
+type OfficeRestrictionNotificationEvent =
     {
         location: string
     }
 
-[<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]
-()
-
+[<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]()
 type OfficeRestrictionNotificationsHandler() =
     member _.Handle (event: JsonDocument) = task {
         let baseUrl = "https://api.dev.book-a-desk.broadsign.net" // To inject
-        let eventDeserialized = JsonSerializer.Deserialize<MyEvent>(event)
+        let eventDeserialized = JsonSerializer.Deserialize<OfficeRestrictionNotificationEvent>(event)
         let officeId = Office.getOfficeById eventDeserialized.location baseUrl
         let! sendNotification = OfficeRestrictions.sendOfficeRestrictionsNotifications officeId baseUrl |> Async.AwaitTask
         printfn $"Notification successfully sent for location {eventDeserialized.location} with OfficeId {officeId}"
