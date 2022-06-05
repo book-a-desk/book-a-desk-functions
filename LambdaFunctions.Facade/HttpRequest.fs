3da7eee29ee5a767a61ae618e79967aaa48030e3
Steve Renard <steve.renard@broadsign.com>
2022-06-05 18:42:29 +0200

    Removed indentation

--- LambdaFunctions.Facade/HttpRequest.fs
+++ LambdaFunctions.Facade/HttpRequest.fs
@@ -4,18 +4,18 @@ open System.Net.Http
 open System.Text
 
 let getAsyncGetContent (url : string) = async {
-        let httpClient = new HttpClient()
-        let! response = httpClient.GetAsync(url) |> Async.AwaitTask
-        response.EnsureSuccessStatusCode () |> ignore
-        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
-        return content
-    }
+    let httpClient = new HttpClient()
+    let! response = httpClient.GetAsync(url) |> Async.AwaitTask
+    response.EnsureSuccessStatusCode () |> ignore
+    let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+    return content
+}
     
 let postAsyncGetContent (url : string) content = async {
-        let httpClient = new HttpClient()
-        let content = new StringContent(content, Encoding.UTF8, "application/json")
-        let! response = httpClient.PostAsync(url, content) |> Async.AwaitTask
-        response.EnsureSuccessStatusCode () |> ignore
-        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
-        return content
-    }
+    let httpClient = new HttpClient()
+    let content = new StringContent(content, Encoding.UTF8, "application/json")
+    let! response = httpClient.PostAsync(url, content) |> Async.AwaitTask
+    response.EnsureSuccessStatusCode () |> ignore
+    let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+    return content
+}
