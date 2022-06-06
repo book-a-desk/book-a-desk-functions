9a3b81679ad74a8a1bf8f46b67370ef58580cf8e
Steve Renard <steve.renard@broadsign.com>
2022-06-06 12:27:17 +0200

    Build

--- LambdaFunctions.Facade.Tests/OfficeTests.fs
+++ LambdaFunctions.Facade.Tests/OfficeTests.fs
@@ -2,22 +2,22 @@
 
 open System
 open Xunit
+
+let baseUrl = "https://api.dev.book-a-desk.broadsign.net" // TODO: To mock
         
 [<Fact>]
 let ``Given an office location in Canada When calling the offices endpoint Then returns office Id`` () = async {
-    let baseUrl = "https://api.dev.book-a-desk.broadsign.net" // TODO: To mock
     let officeLocation = "Montreal"
     let expectedOfficeId = "4b774d13-645b-4378-a925-1da565a35fd7"
-    let! officeId = Office.getOfficeById officeLocation baseUrl
+    let! officeId = LambdaFunctions.Facade.Office.getOfficeById officeLocation baseUrl
     Assert.Equal(expectedOfficeId, officeId)
 }
 
 [<Fact>]
 let ``Given an office location in Europe When calling the offices endpoint Then returns office Id`` () = async {
-    let baseUrl = "https://api.dev.book-a-desk.broadsign.net" // TODO: To mock
     let officeLocation = "Berlin"
     let expectedOfficeId = "16c3d468-c115-4452-8502-58b821d6640b"
-    let! officeId = Office.getOfficeById officeLocation baseUrl
+    let! officeId = LambdaFunctions.Facade.Office.getOfficeById officeLocation baseUrl
     Assert.Equal(expectedOfficeId, officeId)
 }
 
